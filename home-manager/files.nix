b3b1b3d1df290e6a5895f6b22bd1a2c170101b52
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2024-04-28 22:57:51 -0400

    Updated file paths for config files that are not declared in nix.

--- home-manager/files.nix
+++ home-manager/files.nix
@@ -4,21 +4,21 @@
   "ultisnips" = {
     recursive = true;
     source = config.lib.file.mkOutOfStoreSymlink
-    "${config.home.homeDirectory}/.config/home-manager/nvim/UltiSnips";
+    "${config.home.homeDirectory}/.config/home-manager/nvim.nix/UltiSnips";
     target = ".config/nvim/UltiSnips";
   };
 
   "fastfetch" = {
     recursive = true;
     source = config.lib.file.mkOutOfStoreSymlink
-    "${config.home.homeDirectory}/.config/home-manager/fastfetch/config.jsonc";
+    "${config.home.homeDirectory}/.config/home-manager/fastfetch.nix/config.jsonc";
     target = ".config/fastfetch/config.jsonc";
   };
 
   "vifm" = {
     recursive = true;
     source = config.lib.file.mkOutOfStoreSymlink
-    "${config.home.homeDirectory}/.config/home-manager/vifm/vifmrc";
+    "${config.home.homeDirectory}/.config/home-manager/vifm.nix/vifmrc";
     target = ".config/vifm/vifmrc";
   };
 }
