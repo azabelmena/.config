57cc79e469c2b3fd69315974f055d8aa2c9d74e0
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2024-06-17 16:39:07 -0400

    Deleted vifm config, switching over to lf.

--- home-manager/files.nix
+++ home-manager/files.nix
@@ -14,11 +14,4 @@
     "${config.home.homeDirectory}/.config/home-manager/fastfetch.nix/config.jsonc";
     target = ".config/fastfetch/config.jsonc";
   };
-
-  "vifm" = {
-    recursive = true;
-    source = config.lib.file.mkOutOfStoreSymlink
-    "${config.home.homeDirectory}/.config/home-manager/vifm.nix/vifmrc";
-    target = ".config/vifm/vifmrc";
-  };
 }
