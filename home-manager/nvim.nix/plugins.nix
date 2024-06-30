616c5ce0aeb87c9af3d093dc4ef00280f122645e
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2024-06-30 14:02:43 -0400

    Added image support for nixvim.

--- home-manager/nvim.nix/plugins.nix
+++ home-manager/nvim.nix/plugins.nix
@@ -2,6 +2,7 @@
 
   bufferline = ( import ./plugins/bufferline.nix );
   floaterm = ( import ./plugins/floaterm.nix );
+  image = ( import ./plugins/image.nix );
   lualine = ( import ./plugins/lualine.nix );
   telescope = ( import ./plugins/telescope.nix );
   treesitter = ( import ./plugins/treesitter.nix );
