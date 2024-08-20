c846ff3a334dbb1e702b367c47b914b6aad29491
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2024-08-20 18:55:28 -0400

    Revert "Merge branch 'main' of github.com:azabelmena/.config"
    This reverts commit f84551fe91535e7b7ba5851839f8ae05a49a775e, reversing
    changes made to a940d69e8fba60defc8d068f96436c5d36c493ee.

---
+++ home-manager/kitty.nix
@@ -0,0 +1,29 @@
+{ pkgs, config, ... }:
+{
+
+  package = pkgs.kitty;
+
+  enable = true;
+
+  keybindings = {
+      "ctrl+shift+l" = "next_tab";
+      "ctrl+shift+h" = "previous_tab";
+
+      "ctrl+shift+delete" = "clear_terminal reset active";
+      "ctrl+shift+f" = "toggle_fullscreen";
+      "ctrl+shift+t" = "no_op";
+  };
+
+  settings = {
+      enable_audio_bell = "no";
+      remember_window_size = "yes";
+      confirm_os_window_close = "0";
+      hide_window_decorations = "titlebar-only";
+      cursor_blink_interval = 0;
+
+      macos_hide_from_tasks = "yes";
+      macos_quit_when_last_window_closed = "yes";
+
+      macos_colorspace = "srgb";
+  };
+}
