eca0d8579d901a7085581b40f329944738340f55
Diego A. Estrada Rivera <daestrada@pm.me>
2024-08-20 09:39:34 -0400

    Fixed bug

--- home-manager/files.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, config, ... }:
-{
-  "ultisnips" = {
-    recursive = true;
-    source = config.lib.file.mkOutOfStoreSymlink
-    "${config.home.homeDirectory}/.config/home-manager/nvim.nix/UltiSnips";
-    target = ".config/nvim/UltiSnips";
-  };
-}
