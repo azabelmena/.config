2999a4b705a84af02edf1fa46cad3179e1b97f57
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2024-08-25 16:21:49 -0400

    Updated services for cayley.

--- nix/home-manager/hosts/cayley/services.nix
+++ nix/home-manager/hosts/cayley/services.nix
@@ -1,4 +1,6 @@
 { pkgs, config, ... }:
 {
   mako = ( import ../../programs/mako.nix { inherit pkgs config; } );
+  swayidle = ( import ../../programs/swayidle.nix { inherit pkgs config; } );
+  wlsunset = ( import ../../programs/wlsunset.nix { inherit pkgs; } );
 }
