a35c04b8015ae311dfafb0e52d56a16ebb258a3c
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2024-08-25 16:20:59 -0400

    Added sunrise and sunset values to wlsunset.nix

--- nix/home-manager/programs/wlsunset.nix
+++ nix/home-manager/programs/wlsunset.nix
@@ -4,13 +4,9 @@
   package = pkgs.wlsunset;
   enable = true;
 
-  latitude = "18.44";
-  longitude = "66.03";
-
   sunrise = "06:00";
   sunset = "18:30";
 
   gamma = "1.0";
-  systemdTarget = "graphical-session.target";
 
 }
