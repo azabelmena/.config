54a3169dd7e6096ea7d8959c2a0863c65031b174
Alec S. Zabel-Mena <alec@upr.edu>
2024-08-26 15:26:30 -0400

    Declaritively added the aerospace config.

--- nix/modules/nix-darwin/misc/files.nix
+++ nix/modules/nix-darwin/misc/files.nix
@@ -3,7 +3,14 @@
   "ultisnips" = {
     recursive = true;
     source = config.lib.file.mkOutOfStoreSymlink
-    "${config.home.homeDirectory}/.config/home-manager/nvim.nix/UltiSnips";
-    target = ".config/nvim/UltiSnips";
+    "${config.home.homeDirectory}/.config/nix/home-manager/programs/nvim.nix/UltiSnips";
+    target = "${config.home.homeDirectory}/.config/nvim/UltiSnips";
+  };
+
+  "aerospace" = {
+    recursive = true;
+    source = config.lib.file.mkOutOfStoreSymlink
+    "${config.home.homeDirectory}/.config/nix/home-manager/programs/aerospace.toml";
+    target = "${config.home.homeDirectory}/.config/aerospace/aerospace.toml";
   };
 }
