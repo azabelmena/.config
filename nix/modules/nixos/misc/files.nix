92bd838f8af476916db9ed4e951043ac9893bd5f
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2024-08-27 23:49:07 -0400

    Updated pathing to be more declaritive in files.nix, for the nixos hosts (caylye, cauchy, etc).

--- nix/modules/nixos/misc/files.nix
+++ nix/modules/nixos/misc/files.nix
@@ -1,9 +1,9 @@
-{ pkgs, config, ... }:
+{ pkgs, config, lib, ... }:
 {
   "ultisnips" = {
     recursive = true;
     source = config.lib.file.mkOutOfStoreSymlink
-    "${config.home.homeDirectory}/.config/home-manager/nvim.nix/UltiSnips";
-    target = ".config/nvim/UltiSnips";
+    "${config.home.homeDirectory}/.config/nix/home-manager/programs/nvim.nix/UltiSnips";
+    target = "${config.home.homeDirectory}/.config/nvim/UltiSnips";
   };
 }
