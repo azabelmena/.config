bdd50d46712733fce97086b988d484b502acbef5
Alec S. Zabel-Mena <alec@upr.edu>
2024-08-28 19:36:29 -0400

    Configured nix-darwin to also declaritively add apps from the App store.

--- nix/modules/nix-darwin/homebrew.nix
+++ nix/modules/nix-darwin/homebrew.nix
@@ -12,6 +12,13 @@ in
     "tailscale"
   ];
 
+  masApps = {
+    "Goodnotes 6" = 1444383602;
+    "Microsoft Outlook" = 985367838;
+    "Microsoft Word" = 462054704;
+    "WhatsApp Messenger" = 310633997;
+  };
+
   caskArgs = {
     appdir = "${HOME}/Applications";
     require_sha = true;
