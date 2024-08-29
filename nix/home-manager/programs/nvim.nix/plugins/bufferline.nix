7c507e0f4d596f4b63c2a14df2d09a2c4db034bc
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2024-08-29 09:13:44 -0400

    Updated bufferline with the new options.

--- nix/home-manager/programs/nvim.nix/plugins/bufferline.nix
+++ nix/home-manager/programs/nvim.nix/plugins/bufferline.nix
@@ -1,23 +1,19 @@
 {
   enable = true;
 
-  alwaysShowBufferline = false;
-  hover.enabled = false;
-
-  showBufferIcons = true;
-  showBufferCloseIcons = false;
-
-  colorIcons = true;
-
-  tabSize = 16;
-
-  maxNameLength = 16;
-  maxPrefixLength = 8;
-
-  mode = "buffers";
-
-  persistBufferSort = true;
-
-  separatorStyle = "thin";
-
+  settings = {
+    options = {
+      hover.enabled = false;
+      max_name_length = 16;
+      max_prefix_length = 8;
+      persist_buffer_sort = true;
+      always_Show_bufferline = false;
+      show_buffer_icons = true;
+      show_buffer_close_icons = false;
+      color_icons = true;
+      tab_size = 16;
+      mode = "buffers";
+      separator_style = "thin";
+    };
+  };
 }
