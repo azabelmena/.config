afdec4e92ad0424b4fbfb04f409e777a5f5ce5bc
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2024-09-28 20:10:44 -0400

    Removed bufferline plugin from nvim.

--- nix/home-manager/programs/nvim.nix/plugins.nix
+++ nix/home-manager/programs/nvim.nix/plugins.nix
@@ -1,6 +1,5 @@
 {
 
-  bufferline = ( import ./plugins/bufferline.nix );
   floaterm = ( import ./plugins/floaterm.nix );
   image = ( import ./plugins/image.nix );
   lualine = ( import ./plugins/lualine.nix );
