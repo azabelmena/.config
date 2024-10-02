a3b057f4db6cfd167bbba3591a79c23e071e1752
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2024-10-02 01:08:41 -0400

    Updated xdg directories and included new default dirs to be created.

--- nix/modules/nixos/misc/xdg.nix
+++ nix/modules/nixos/misc/xdg.nix
@@ -30,17 +30,21 @@ in
     download = "${home}/Downloads/";
     music = "${home}/Music/";
     pictures = "${home}/Pictures/";
-    videos = "${home}/Videos/";
     templates = "${home}/Templates/";
+    videos = "${home}/Videos/";
 
     extraConfig = {
+      XDG_DEVNIX_DIR = "${home}/devnix/";
+      XDG_GAMES_DIR = "${home}/storage/";
+      XDG_MATHEMATICS_DIR = "${home}/mathematics/";
       XDG_MISC_DIR = "${home}/Misc/";
-      XDG_SCREENSHOTS_DIR = "${home}/Pictures/Screenshots/";
-      XDG_MOUNT_DIR = "${home}/mount/";
       XDG_MOUNT0_DIR = "${home}/mount/mount0/";
       XDG_MOUNT1_DIR = "${home}/mount/mount1/";
       XDG_MOUNT2_DIR = "${home}/mount/mount2/";
       XDG_MOUNT3_DIR = "${home}/mount/mount3/";
+      XDG_MOUNT_DIR = "${home}/mount/";
+      XDG_SCREENSHOTS_DIR = "${home}/Pictures/Screenshots/";
+      XDG_STORAGE_DIR = "${home}/storage/";
     };
   };
 }
