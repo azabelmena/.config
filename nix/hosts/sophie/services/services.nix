54833a709dc3540f428c6e8b00da1bd0b914667c
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2024-10-02 19:42:53 -0400

    Streamlined how services are managed on sophie.

--- nix/hosts/sophie/services/services.nix
+++ nix/hosts/sophie/services/services.nix
@@ -1,26 +1,26 @@
 { pkgs, ... }:
 {
-  acpid = ( import ./acpid.nix );
+  acpid = ( import ../../../modules/nixos/services/acpid.nix );
 
-  avahi = ( import ./avahi.nix );
+  avahi = ( import ../../../modules/nixos/services/avahi.nix );
 
-  displayManager = ( import ./displayManager.nix { inherit pkgs; } );
+  displayManager = ( import ../../../modules/nixos/services/displayManager.nix { inherit pkgs; } );
 
-  fprintd = ( import ./fprintd.nix { inherit pkgs; } );
+  fprintd = ( import ../../../modules/nixos/services/fprintd.nix { inherit pkgs; } );
 
-  fwupd = ( import ./fwupd.nix { inherit pkgs; } );
-
-  tlp = ( import ../../../modules/nixos/services/tlp.nix );
+  fwupd = ( import ../../../modules/nixos/services/fwupd.nix { inherit pkgs; } );
 
   logind = ( import ../../../modules/nixos/services/logind.nix );
 
   openssh = ( import ./ssh.nix );
 
-  pipewire = ( import ./pipewire.nix );
+  pipewire = ( import ../../../modules/nixos/services/pipewire.nix );
 
-  printing = ( import ./printing.nix );
+  printing = ( import ../../../modules/nixos/services/printing.nix );
 
   tailscale = ( import ./tailscale.nix { inherit pkgs; } );
+
+  tlp = ( import ../../../modules/nixos/services/tlp.nix );
 
   xserver = ( import ./xserver.nix );
 }
