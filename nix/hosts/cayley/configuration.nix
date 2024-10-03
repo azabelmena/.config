5fbf9fb50fbac65784d995fe62472bab1c6564db
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2024-10-03 11:39:10 -0400

    Pointed programs to new location.

--- nix/hosts/cayley/configuration.nix
+++ nix/hosts/cayley/configuration.nix
@@ -70,7 +70,7 @@
 
   users = ( import ./users/users.nix { inherit pkgs config; } );
 
-  programs = ( import ../../modules/nixos/programs.nix { inherit pkgs; } );
+  programs = ( import ./programs.nix { inherit pkgs; } );
 
   fonts.packages = with pkgs; [
      (nerdfonts.override {fonts  = ["IBMPlexMono"];})
