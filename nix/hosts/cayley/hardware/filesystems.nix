df4d898afa0572a3e5ed5930f48c4bb8f2338575
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2024-10-05 23:17:26 -0400

    Repathed mount point for games.

--- nix/hosts/cayley/hardware/filesystems.nix
+++ nix/hosts/cayley/hardware/filesystems.nix
@@ -11,14 +11,8 @@
       fsType = "ext4";
     };
 
-  #"/mnt/storage" =
-    #{ device = "/dev/disk/by-uuid/a2b80594-d3f1-4291-af41-521667e8f484";
-      #fsType = "ext4";
-    #};
-
-  "/mnt/games" =
-    {
-      device = "/dev/disk/by-uuid/747a30ab-b445-4f3b-a276-67da3eff89a8";
-      fsType = "ext4";
-    };
+  "/home/alec/games" = {
+    device = "/dev/disk/by-uuid/747a30ab-b445-4f3b-a276-67da3eff89a8";
+    fsType = "ext4";
+  };
 }
