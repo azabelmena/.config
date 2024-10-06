059824edddb567a01948dd3c1264e6ae9c1992ec
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2024-10-05 23:18:32 -0400

    Fixed error in declaration of hypridle where the timeout ignored the hyprlock builtin timeout.

--- nix/home-manager/programs/hypr.nix/hypridle.nix
+++ nix/home-manager/programs/hypr.nix/hypridle.nix
@@ -8,13 +8,13 @@
     general = {
       before_sleep_cmd = "pidof hyprlock || hyprlock --immediate --immediate-render";
       ignore_dbus_inhibit = false;
-      lock_cmd = "pidof hyprlock || hyprlock --immediate --immediate-render";
+      lock_cmd = "pidof hyprlock || hyprlock";
     };
 
     listener = [
       {
         timeout = 256;
-        on-timeout = "pidof hyprlock || hyprlock --immediate --immediate-render";
+        on-timeout = "pidof hyprlock || hyprlock";
       }
       {
         timeout = 512;
