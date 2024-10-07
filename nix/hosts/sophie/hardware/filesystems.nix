85f7952f6d099676500cc85a3b6447da0a74c21d
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2024-10-06 23:53:17 -0400

    Removing storage from mount for testing purposes.

--- nix/hosts/sophie/hardware/filesystems.nix
+++ nix/hosts/sophie/hardware/filesystems.nix
@@ -11,9 +11,9 @@
     options = [ "fmask=0077" "dmask=0077" ];
   };
 
-  "/home/alec/storage" = {
-    device = "/dev/disk/by-uuid/fd290d04-7308-4530-ae12-1c89493f153d";
-    fsType = "ext4";
-  };
+  #"/home/alec/storage" = {
+    #device = "/dev/disk/by-uuid/fd290d04-7308-4530-ae12-1c89493f153d";
+    #fsType = "ext4";
+  #};
 
 }
