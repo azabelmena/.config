fab8e7eedbbc4cb0ae044606293fcc5a00a3813a
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2024-10-10 18:01:03 -0400

    Updated textwidth.

--- nix/home-manager/programs/nvim.nix/options.nix
+++ nix/home-manager/programs/nvim.nix/options.nix
@@ -10,14 +10,14 @@
   softtabstop = 2;
   tabstop = 2;
 
-  colorcolumn = "77";
+  colorcolumn = "76";
   cursorcolumn = true;
   cursorline = true;
 
   conceallevel = 1;
 
   backup = false;
-  textwidth = 80;
+  textwidth = 75;
   undodir = "${config.home.homeDirectory}/.vim/undodir";
   undofile = true;
   wrap = false;
