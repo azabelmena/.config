78d8554ddeed5935c619bcefc4cd27310b72cd03
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2024-10-16 11:19:12 -0400

    Set hyprlock to ignore empty inputs.

--- nix/home-manager/programs/hypr.nix/hyprlock.nix
+++ nix/home-manager/programs/hypr.nix/hyprlock.nix
@@ -19,7 +19,7 @@ in
       hide_cursor = true;
       no_fade_in = false;
       no_fade_out = false;
-      ignore_empty_input = false;
+      ignore_empty_input = true;
       pam_module = "hyprlock";
       fractional_scaling = 2;
     };
