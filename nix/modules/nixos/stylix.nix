18c5009746e96670b64dd9de63f797c313345987
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2024-10-26 01:29:44 -0400

    Updated fonts for scaling across different monitors (this probably still fucks with sophie). Also removed plymouth target (I raw dog the systemd boot).

--- nix/modules/nixos/stylix.nix
+++ nix/modules/nixos/stylix.nix
@@ -39,10 +39,10 @@ in
     };
 
     sizes = {
-      applications = 12;
-      desktop = 12;
-      popups = 12;
-      terminal = 12;
+      applications = 18;
+      desktop = 18;
+      popups = 18;
+      terminal = 18;
     };
   };
 
@@ -50,10 +50,6 @@ in
     package = google-cursor;
     name = "GoogleDot-Black";
     size = 24;
-  };
-
-  targets = {
-    plymouth.enable = false;
   };
 
 }
