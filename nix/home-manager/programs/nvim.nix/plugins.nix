4d950691be8b93e3c3a6d1e9d487a1c8b0c58718
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2024-11-15 22:59:12 -0400

    Fixed typo.

--- nix/home-manager/programs/nvim.nix/plugins.nix
+++ nix/home-manager/programs/nvim.nix/plugins.nix
@@ -3,5 +3,5 @@
   lualine = ( import ./plugins/lualine.nix { inherit pkgs; } );
   telescope = ( import ./plugins/telescope.nix { inherit pkgs; } );
   treesitter = ( import ./plugins/treesitter.nix { inherit pkgs; } );
-  web-devicons = ( import ./plugins/devicons.nix { inherit pkgs; } )
+  web-devicons = ( import ./plugins/devicons.nix { inherit pkgs; } );
 }
