2ee673dd1ae344cd9b94894a4a9437b652190a81
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2025-01-03 12:34:11 -0400

    Added sensible defualt configuration to ghostty.

--- nix/home-manager/programs/ghostty.nix
+++ nix/home-manager/programs/ghostty.nix
@@ -6,7 +6,23 @@
 
   settings = {
     title = "I Love being Ghostted";
+    auto-update = "off";
+    confirm-close-surface = false;
+
     gtk-titlebar = false;
     gtk-adwaita = false;
+
+    mouse-hide-while-typing = true;
+
+    scrollback-limit = 500;
+
+    link-url = true;
+
+    window-vsync = true;
+
+    quit-after-last-window-closed = true;
+
+    shell-integration = "zsh";
+    shell-integration-features = "cursor, title, sudo";
   };
 }
