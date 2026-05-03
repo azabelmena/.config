4f836674adb144f424805505cdbd848f956bff0f
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2026-05-03 11:06:41 -0400

    Fixed navidrome issue (rip my playlists).

--- nix/hosts/cayley/services/navidrome.nix
+++ nix/hosts/cayley/services/navidrome.nix
@@ -9,8 +9,6 @@
   settings = {
     AutoImportPlaylists = true;
     MusicFolder = "/home/alec/Audio/Music";
-    DataFolder = "/home/alec/.local/share/navidrome";
-    PlaylistsPath = "/home/alec/.local/share/navidrome/playlists";
     Address = "100.125.95.69";
     Port = 4533;
     Scanner = {
