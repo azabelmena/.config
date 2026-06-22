48cb3ce4156bc52191560e4e191afce83d585ff1
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2026-06-22 01:13:57 -0400

    More work on niri. Should be in a usable state soon. Need to figure out how to get it working with the greeter.

--- nix/home-manager/programs/niri.nix
+++ nix/home-manager/programs/niri.nix
@@ -1,16 +1,48 @@
 { pkgs, ... }:
 let
-  mod = "Alt";
+mod = "Super";
+exit = "Ctrl+Alt+Delete";
 in
 {
   enable = true;
   package = pkgs.niri;
 
   settings = {
+    prefer-no-csd = true;
+
+    hotkey-overlay = {
+      skip-at-startup = true;
+    };
+
+    input = {
+      focus-follows-mouse.enable = true;
+    };
+
+    outputs = {
+      "HDMI-A-1" = {
+        mode = {
+          width = 1920;
+          height = 1080;
+          refresh = 60.000;
+        };
+        scale = 1.0;
+        position = {
+          x = 0;
+          y = 0;
+        };
+      };
+    };
+
+    spawn-at-startup = [
+      { argv = [ "noctalia" ]; }
+    ];
+
     binds = {
       "${mod}+Return".action.spawn = "foot";
-      "${mod}+B".action.spawn = "qutebrowser";
-      "${mod}+Space".action.spawn = "fuzzel";
+      "${mod}+b".action.spawn = "qutebrowser";
+      "${mod}+Space".action.spawn = "noctalia msg panel-toggle launcher";
+      "${mod}+q".action.close-window = {};
+      "${exit}".action.quit.skip-confirmation = true;
     };
   };
 }
