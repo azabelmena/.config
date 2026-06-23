97c79637efc9a7f5ff598b774917abb6156dc94c
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2026-06-23 15:19:46 -0400

    Adding back workspaces.

--- nix/home-manager/programs/hypr.nix/hyprland.nix
+++ nix/home-manager/programs/hypr.nix/hyprland.nix
@@ -106,40 +106,52 @@
 
     bind = $mod_main, R, exec, hyprlock reload
     bind = $mod_main, Q, killactive,
-         bind = $mod_main, DELETE, exec, noctalia msg session lock
-           bind = $mod_exit, DELETE, exit
+    bind = $mod_main, DELETE, exec, noctalia msg session lock
+    bind = $mod_exit, DELETE, exit
 
-           bind = $mod_main, T, togglefloating,
-         bind = $mod_main, P, pseudo, # dwindle
-           bind = $mod_main, TAB, cyclenext
-           bind = $mod_shift, TAB, cyclenext, prev
-           bind = $mod_main, F, fullscreen
+    bind = $mod_main, T, togglefloating,
+    bind = $mod_main, P, pseudo, # dwindle
+    bind = $mod_main, TAB, cyclenext
+    bind = $mod_shift, TAB, cyclenext, prev
+    bind = $mod_main, F, fullscreen
 
-           bind = $mod_main, h, movefocus, l
-           bind = $mod_main, j, movefocus, d
-           bind = $mod_main, k, movefocus, u
-           bind = $mod_main, l, movefocus, r
+    bind = $mod_main, h, movefocus, l
+    bind = $mod_main, j, movefocus, d
+    bind = $mod_main, k, movefocus, u
+    bind = $mod_main, l, movefocus, r
 
-           bind = $mod_shift, h, swapwindow, l
-           bind = $mod_shift, j, swapwindow, d
-           bind = $mod_shift, k, swapwindow, u
-           bind = $mod_shift, l, swapwindow, r
+    bind = $mod_shift, h, swapwindow, l
+    bind = $mod_shift, j, swapwindow, d
+    bind = $mod_shift, k, swapwindow, u
+    bind = $mod_shift, l, swapwindow, r
 
-           bind = $mod_main SHIFT, 1, movetoworkspacesilent, 1
-           bind = $mod_main SHIFT, 2, movetoworkspacesilent, 2
-           bind = $mod_main SHIFT, 3, movetoworkspacesilent, 3
-           bind = $mod_main SHIFT, 4, movetoworkspacesilent, 4
-           bind = $mod_main SHIFT, 5, movetoworkspacesilent, 5
-           bind = $mod_main SHIFT, 6, movetoworkspacesilent, 6
-           bind = $mod_main SHIFT, 7, movetoworkspacesilent, 7
-           bind = $mod_main SHIFT, 8, movetoworkspacesilent, 8
-           bind = $mod_main SHIFT, 9, movetoworkspacesilent, 9
-           bind = $mod_main SHIFT, 0, movetoworkspacesilent, 10
+    bind = $mod_main, 1, workspace, 1
+    bind = $mod_main, 2, workspace, 2
+    bind = $mod_main, 3, workspace, 3
+    bind = $mod_main, 4, workspace, 4
+    bind = $mod_main, 5, workspace, 5
+    bind = $mod_main, 6, workspace, 6
+    bind = $mod_main, 7, workspace, 7
+    bind = $mod_main, 8, workspace, 8
+    bind = $mod_main, 9, workspace, 9
+    bind = $mod_main, 0, workspace, 10
 
-           bind = $mod_main, mouse_down, workspace, e+1
-           bind = $mod_main, mouse_up, workspace, e-1
+    bind = $mod_main SHIFT, 1, movetoworkspacesilent, 1
+    bind = $mod_main SHIFT, 2, movetoworkspacesilent, 2
+    bind = $mod_main SHIFT, 3, movetoworkspacesilent, 3
+    bind = $mod_main SHIFT, 4, movetoworkspacesilent, 4
+    bind = $mod_main SHIFT, 5, movetoworkspacesilent, 5
+    bind = $mod_main SHIFT, 6, movetoworkspacesilent, 6
+    bind = $mod_main SHIFT, 7, movetoworkspacesilent, 7
+    bind = $mod_main SHIFT, 8, movetoworkspacesilent, 8
+    bind = $mod_main SHIFT, 9, movetoworkspacesilent, 9
+    bind = $mod_main SHIFT, 0, movetoworkspacesilent, 10
 
-           bindm = $mod_main, mouse:272, movewindow
-           bindm = $mod_main, mouse:273, resizewindow
-           '';
+
+    bind = $mod_main, mouse_down, workspace, e+1
+    bind = $mod_main, mouse_up, workspace, e-1
+
+    bindm = $mod_main, mouse:272, movewindow
+    bindm = $mod_main, mouse:273, resizewindow
+  '';
 }
