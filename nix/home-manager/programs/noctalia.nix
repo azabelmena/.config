dc8dc04f888a5b6d59c86e887c53337ad98bd13d
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2026-06-26 00:44:08 -0400

    Restructured noctalia.nix based on an actual example config.

--- nix/home-manager/programs/noctalia.nix
+++ nix/home-manager/programs/noctalia.nix
@@ -16,143 +16,13 @@ in
   enable = true;
 
   settings = {
+
     theme = {
       mode = "dark";
       source = "builtin";
       builtin = "Gruvbox";
     };
 
-    bar = {
-      order = [
-        "side_left" "side_right" "widgets" "bottom"
-      ];
-
-      "side_left" = {
-        enabled = true;
-        position = "left";
-        start = [];
-        center = [];
-        end = [];
-        margin_edge = 0;
-        margin_ends = 0;
-        radius = 0;
-        thickness = 10;
-      };
-
-      "side_right" = {
-        enabled = true;
-        position = "right";
-        start = [];
-        center = [];
-        end = [];
-        margin_edge = 0;
-        margin_ends = 0;
-        radius = 0;
-        thickness = 10;
-      };
-
-      widgets = {
-        enabled = true;
-        position = "top";
-        start = [
-          "nixOS_logo" "workspaces" "active_window"
-        ];
-        center = [
-          "cat" "clock" "cat_2"
-        ];
-        end = [
-          "screenshot"
-            "notifications" "clipboard" "network"
-            "bluetooth" "volume" "brightness"
-            "battery" "session"
-        ];
-        font_family = "BlexMono Nerd Font";
-        font_weight = 400;
-        margin_edge = 0;
-        margin_ends = 0;
-        radius = 0;
-        radius_bottom_left = -80;
-        radius_bottom_right = -80;
-        scale = 1.25;
-        thickness = 25;
-        widget_spacing = 10;
-      };
-      "bottom" = {
-        enabled = true;
-        position = "bottom";
-
-        monitor = {
-          "HDMI-A-1" = {
-            match = "HDMI-A-1";
-            start = [
-              "cpu" "ram" "cpu_temp"
-                "disk0" "disk1" "disk2" "disk3"
-            ];
-            center = [ "audio_visualizer" ];
-            end = [ "net_up" "net_down" ];
-          };
-          "eDP-1" = {
-            match = "eDP-1";
-            start = [
-              "cpu" "ram" "cpu_temp"
-                "disk0" "disk4"
-            ];
-            center = [ "audio_visualizer" ];
-            end = [ "net_up" "net_down" ];
-          };
-        };
-
-        font_family = "BlexMono Nerd Font";
-        font_weight = 400;
-        margin_edge = 0;
-        margin_ends = 0;
-        radius = 0;
-        radius_top_left = -80;
-        radius_top_right = -80;
-        scale = 1.25;
-        thickness = 25;
-        widget_spacing = 10;
-      };
-    };
-
-    system.monitor = {
-      enabled = true;
-      cpu_poll_seconds = 2.0;
-      gpu_poll_seconds = 2.0;
-      memory_poll_seconds = 2.0;
-      network_poll_seconds = 3.0;
-      disk_poll_seconds = 60.0;
-    };
-
-    calendar = {
-      enabled = true;
-    };
-
-    location = {
-      auto_locate = false;
-      address = "Carolina, PR";
-      longitude = 18.3814441;
-      latitude = 65.9560489;
-    };
-
-    weather = {
-      enabled = true;
-      refresh_minutes = 30;
-      unit = "metric";
-      effects = true;
-    };
-
-    nightlight = {
-      enabled = true;
-      force = false;
-
-      temperature_day = "6500";
-      temperature_night = "4000";
-    };
-
-    plugins = {
-      enabled = [ "noctalia/bongocat" "noctalia/screen_recorder" "noctalia/timer" ];
-    };
 
     shell = {
       clipboard_history_max_entries = 20;
@@ -204,16 +74,6 @@ in
         }
         ];
       };
-
-      lockscreen = {
-        enabled = true;
-        fingerprint = true;
-        allow_empty_password = false;
-        blurred_desktop = true;
-        blur_intensity = 0.5;
-        tint_intensity = 0;
-        wallpaper = "${background}";
-      };
     };
 
     wallpaper = {
@@ -224,67 +84,161 @@ in
       };
     };
 
-    widget = {
+    backdrop = {
+      enabled = false;
+      blur_intensity = 0.5;
+    };
 
-      active_window = {
-        min_length = 80;
-        max_length = 260;
-        icon_size = 14;
-        title_scroll = true;
-        display = "icon_and_text";
-        show_empty_label = false;
+    lockscreen = {
+        enabled = true;
+        fingerprint = true;
+        allow_empty_password = false;
+        blurred_desktop = true;
+        blur_intensity = 0.5;
+        tint_intensity = 0;
+        wallpaper = "${background}";
+    };
+
+    #notification = {};
+
+    #osd = {};
+
+    system = {
+      monitor = {
+        enabled = true;
+        cpu_poll_seconds = 2.0;
+        gpu_poll_seconds = 2.0;
+        memory_poll_seconds = 2.0;
+        network_poll_seconds = 3.0;
+        disk_poll_seconds = 60.0;
+      };
+    };
+
+    calendar = {
+      enabled = true;
+    };
+
+    location = {
+      auto_locate = false;
+      address = "Carolina, PR";
+      longitude = 18.3814441;
+      latitude = 65.9560489;
+    };
+
+    nightlight = {
+      enabled = true;
+      force = false;
+
+      temperature_day = "6500";
+      temperature_night = "4000";
+    };
+
+    weather = {
+      enabled = true;
+      refresh_minutes = 30;
+      unit = "metric";
+      effects = true;
+
+    };
+
+    #audio = {
+    #};
+
+    #idle = {};
+
+    bar = {
+      order = [ "side_left" "side_right" "top" "bottom" ];
+
+      side_left = {
+        enabled = true;
+        position = "left";
+        start = [];
+        center = [];
+        end = [];
+        margin_edge = 0;
+        margin_ends = 0;
+        radius = 0;
+        thickness = 10;
       };
 
-      audio_visualizer = {
-        bands = 128;
-        color_1 = "primary";
-        color_2 = "secondary";
-        show_when_idle = true;
-        width = 400;
+      side_right = {
+        enabled = true;
+        position = "right";
+        start = [];
+        center = [];
+        end = [];
+        margin_edge = 0;
+        margin_ends = 0;
+        radius = 0;
+        thickness = 10;
       };
 
-      screenshot = {
-        glyph = "camera";
-        primary_click = "fullscreen";
+      top = {
+        enabled = true;
+        position = "top";
+        start = [
+          "nixOS_logo" "workspaces" "active_window"
+        ];
+        center = [
+          "cat" "clock" "cat_2"
+        ];
+        end = [
+          "screenshot"
+            "notifications" "clipboard" "network"
+            "bluetooth" "volume" "brightness"
+            "battery" "session"
+        ];
+        font_family = "BlexMono Nerd Font";
+        font_weight = 400;
+        margin_edge = 0;
+        margin_ends = 0;
+        radius = 0;
+        radius_bottom_left = -80;
+        radius_bottom_right = -80;
+        scale = 1.25;
+        thickness = 25;
+        widget_spacing = 10;
       };
 
-      cat = {
-        audio_spectrum = true;
-        rave_mode = true;
-        tappy_mode = true;
-        type = "noctalia/bongocat:cat";
-        use_mpris_filter = true;
-      };
-      clock = {
-        format = "{:%H.%M.%S}";
-      };
-      cat_2 = {
-        audio_spectrum = true;
-        rave_mode = true;
-        tappy_mode = true;
-        type = "noctalia/bongocat:cat";
-        use_mpris_filter = true;
-      };
+      bottom = {
+        enabled = true;
+        position = "bottom";
 
-      nixOS_logo = {
-        color = "primary";
-        glyph = "";
-        icon_color = "primary";
-        label = "";
-        type = "custom_button";
+        monitor = {
+          "HDMI-A-1" = {
+            match = "HDMI-A-1";
+            start = [
+              "cpu" "ram" "cpu_temp"
+                "disk0" "disk1" "disk2" "disk3"
+            ];
+            center = [ "audio_visualizer" ];
+            end = [ "net_up" "net_down" ];
+          };
+          "eDP-1" = {
+            match = "eDP-1";
+            start = [
+              "cpu" "ram" "cpu_temp"
+                "disk0" "disk4"
+            ];
+            center = [ "audio_visualizer" ];
+            end = [ "net_up" "net_down" ];
+          };
+        };
+
+        font_family = "BlexMono Nerd Font";
+        font_weight = 400;
+        margin_edge = 0;
+        margin_ends = 0;
+        radius = 0;
+        radius_top_left = -80;
+        radius_top_right = -80;
+        scale = 1.25;
+        thickness = 25;
+        widget_spacing = 10;
       };
-      cpu= {
-        type = "sysmon";
-        stat = "cpu_usage";
-      };
-      gpu = {
-        type = "sysmon";
-        stat = "gpu_usage";
-      };
-      ram = {
-        type = "sysmon";
-        stat = "ram_used";
-      };
+    };
+
+    widgets = {
       cpu_temp = {
         type = "sysmon";
         stat = "cpu_temp";
@@ -323,7 +277,7 @@ in
         glyph = "database";
         stat = "disk_pct";
         path = "/home/alec/storage";
-      };
+            };
 
       net_up = {
         type = "sysmon";
@@ -338,6 +292,17 @@ in
         type = "battery";
         device = "BAT1";
       };
+
     };
+
+    plugins = {
+      enabled = [
+        "noctalia/bongocat"
+          "noctalia/screen_recorder"
+          "noctalia/timer"
+          "noctalia/translator"
+      ];
+    };
+
   };
 }
