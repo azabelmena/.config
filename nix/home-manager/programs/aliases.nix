a31e4698afd7141b34982879b93a749824aab7df
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2026-08-12 12:20:07 -0400

    Updated alias.

--- nix/home-manager/programs/aliases.nix
+++ nix/home-manager/programs/aliases.nix
@@ -4,7 +4,7 @@
   clear = "(clear) && (${pkgs.fortune}/bin/fortune | ${pkgs.neo-cowsay}/bin/cowsay --random)";
   #clear="(clear) && (cat ${config.home.homeDirectory}/logo.txt)"; # sfw version of clear alias
   ls="${pkgs.eza}/bin/eza";
-  bat="${pkgs.bat}/bin/bat --theme gruvbox-dark";    # Add bat alternative to less.
+  cat="${pkgs.bat}/bin/bat --theme gruvbox-dark";    # Add bat alternative to less.
 
   vi = "nvim -u ${config.home.homeDirectory}/.config/nvim/init.lua";
   vim = "nvim -u ${config.home.homeDirectory}/.config/nvim/init.lua";
